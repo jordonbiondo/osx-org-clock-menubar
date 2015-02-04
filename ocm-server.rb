783dfec87e30147e9fb8df81e8944e4120e73828
jordonbiondo <biondoj@mail.gvsu.edu>
2015-02-04 10:22:03 -0500

    macruby server rewrite

--- ocm-server.rb
+++ ocm-server.rb
@@ -3,54 +3,127 @@
 # Thank you rubiojr: https://gist.github.com/rubiojr/245402
 
 framework 'AppKit'
-require 'socket'
 
-@status_item = nil
-@app = nil
-@port = ARGV[0].nil? ? 65432 : ARGV[0].to_i
+# Handles drawing the menubar
+class OCMView
+  def initialize(app)
+    @app = app
+    menu = setup_menu
+    init_status_bar(menu)
+    @attrs_dictionary = NSDictionary.dictionaryWithObject(
+      NSFont.fontWithName('LucidaGrande', size: 11.0),
+      forKey: NSFontAttributeName
+    )
+  end
 
-def setup_menu
-  menu = NSMenu.new
-  menu.initWithTitle 'Org Clock Menubar Server'
-  mi = NSMenuItem.new
-  mi.title = 'Quit Org Clock Menubar Server'
-  mi.action = 'stop:'
-  mi.target = self
-  menu.addItem mi
-  menu
+  def set_text(text)
+    @attr_string = NSAttributedString.alloc.initWithString(text, attributes: @attrs_dictionary)
+    @status_item.setHighlightMode(:YES)
+    @status_item.setAttributedTitle(@attr_string)
+  end
+
+  private
+
+  attr_accessor :app
+
+  def setup_menu
+    menu = NSMenu.new
+    menu.initWithTitle 'Org Clock Menubar Server'
+    mi = NSMenuItem.new
+    mi.title = 'Quit Org Clock Menubar Server'
+    mi.action = 'stop_action:'
+    mi.target = @app.delegate
+    menu.addItem mi
+    menu
+  end
+
+  def init_status_bar(menu)
+    status_bar = NSStatusBar.systemStatusBar
+    @status_item = status_bar.statusItemWithLength(-1)
+    @status_item.setMenu menu
+    @status_item.setTitle('')
+    @status_item.setToolTip('Org Clock Menubar Server')
+  end
 end
 
-def init_status_bar(menu)
-  status_bar = NSStatusBar.systemStatusBar
-  @status_item = status_bar.statusItemWithLength(-1)
-  @status_item.setMenu menu
-  @status_item.setTitle("")
-  @status_item.setToolTip("Org Task Tracker")
-end
+# Handles client communication
+class OCMModel
+  require 'socket'
+  attr_accessor :server
 
-def stop(sender)
-  @app.terminate(nil)
-end
+  def initialize(port)
+    @port = port
+  end
 
-def start_server
-  puts @port
-  ss = TCPServer.new(@port)
-  Thread.new do
-    loop do
-      Thread.start(ss.accept) { |s|
-        begin
-          while line = s.gets
-            @status_item.setTitle(line.strip!) if @status_item
+  def on_change(&block)
+    @on_change_hook = block
+  end
+
+  def start!
+    start_server
+  end
+
+  private
+
+  attr_accessor :on_change_hook, :main_thread, :port
+
+  def start_server
+    return if @main_thread && @main_thread.alive?
+    @server = TCPServer.new(@port)
+    @main_thread = Thread.new do
+      loop do
+        Thread.start(@server.accept) { |s|
+          begin
+            while line = s.gets
+              @on_change_hook.call(line.strip!) if @on_change_hook
+            end
+          ensure
+            s.close
           end
-        ensure
-          s.close
-        end
-      }
+        }
+      end
     end
   end
 end
 
-@app = NSApplication.sharedApplication
-init_status_bar(setup_menu)
-start_server
-@app.run
+class OCMDelegate
+  attr_accessor :app
+
+  def initialize(app, view, model)
+    @app = app
+    view_model_connections(view, model)
+  end
+
+  def stop_action(sender)
+    @app.terminate(nil)
+  end
+
+  private
+
+  def view_model_connections(view, model)
+    model.on_change do |line|
+      view.set_text(line)
+    end
+  end
+
+end
+
+class OCMApplication
+  attr_accessor :app, :view, :model
+
+  def initialize(port)
+    @app = NSApplication.sharedApplication
+    @view = OCMView.new(@app)
+    @model = OCMModel.new(port)
+    @app.delegate = OCMDelegate.new(@app, @view, @model)
+  end
+
+  def run!
+    @model.start!
+    @app.run
+  end
+end
+
+@port = ARGV[0].nil? ? 65_432 : ARGV[0].to_i
+ocm_server = OCMApplication.new(@port)
+ocm_server.run!
